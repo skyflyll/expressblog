940416f2bc5213180feb1d2cde9315c4a6912dc3
linlang <270901715@qq.com>
2018-05-11 17:30:41 +0800

    admin add

--- routes/admin.js
+++ routes/admin.js
@@ -1,37 +1,44 @@
+'sue strict'
 var express = require('express');
 var router = express.Router();
 var chalk = require('chalk');
-var AdminModule = require('../controller/admin')
+var sha1 = require('sha1');
+var AdminModule = require('../controller/admin');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 // 注册admin
-router.get('/create',function(req,res,next){
+router.get('/create', function (req, res, next) {
   // res.send('index')
   var admin = {
-    user_name:'linlang',
-    password:'linlang',
-    id:5,
-    create_time:'2018-05-11',
-    admin:'管理员',
-    status:6,
-    avatar:'icon.jpg',
-    city:'贵阳',
+    user_name: 'linlang',
+    password: 'linlang',
+    id: 5,
+    create_time: '2018-05-11',
+    admin: '管理员',
+    status: 6,
+    avatar: 'icon.jpg',
+    city: '贵阳',
   }
-  AdminModule.create(admin).then((res)=>{
+  AdminModule.create(admin).then((res) => {
     console.log(chalk.green(res))
   });
   res.send('hhhhhhhhhh')
 })
 
-router.get('/name',(req,res,next)=>{
-    var name = 'linlang';
-    AdminModule.getAdminByName(name).then((res)=>{
-        console.log(res)
-    })
+//根据名字获取管理员
+router.get('/name', (req, res, next) => {
+  var name = 'linlang';
+  AdminModule.getAdminByName(name).then((res) => {
+    console.log(res)
+  })
+})
+
+router.get('/login',(req,res,next)=>{
+  
 })
 
 module.exports = router;
