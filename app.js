417a0c433d7d2c76cb9a63a70ad8e966c9b5cf5b
linlang <270901715@qq.com>
2018-05-14 17:33:09 +0800

    cccc

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,8 +10,8 @@ var adminRouter = require('./routes/admin');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var mongo = require('./mongodb/db')
 
+var mongo = require('./mongodb/db')
 
 var app = express();
 
@@ -26,15 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin',adminRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var chalk = require('chalk');
 var sha1 = require('sha1');
 var AdminModule = require('../controller/admin');
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,23 +12,25 @@ router.get('/', function (req, res, next) {
 });
 
 // 注册admin
-router.get('/create', function (req, res, next) {
-  // res.send('index')
-  var admin = {
-    user_name: 'linlang',
-    password: 'linlang',
-    id: 5,
-    create_time: '2018-05-11',
-    admin: '管理员',
-    status: 6,
-    avatar: 'icon.jpg',
-    city: '贵阳',
-  }
-  AdminModule.create(admin).then((res) => {
-    console.log(chalk.green(res))
-  });
-  res.send('hhhhhhhhhh')
+router.get('/create', (req, res) => {
+  res.render('admin', { title: "admin" })
 })
+router.post('/create', function (req, res, next) {
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err,fields,files)=>{
+    if(err){
+      res.send({
+        status:0,
+        type:'FORM_DATA_ERROR',
+        message:'表单信息错误'
+      })
+      return
+    }
+    console.log(fields,files)
+  })
+  res.send('mmmmmmmmmmmmmmmmmmm')
+})
+
 
 //根据名字获取管理员
 router.get('/name', (req, res, next) => {
@@ -37,8 +40,8 @@ router.get('/name', (req, res, next) => {
   })
 })
 
-router.get('/login',(req,res,next)=>{
-  
+router.get('/login', (req, res, next) => {
+
 })
 
 module.exports = router;
